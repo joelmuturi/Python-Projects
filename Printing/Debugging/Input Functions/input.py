6d31b4e53fafd78ae6007c0b5402357e67c33bc9
Joel Karanja <joelkaranja562@gmail.com>
2024-01-12 20:27:25 +0300

    Commit Message

---
+++ Printing/Debugging/Input Functions/input.py
@@ -0,0 +1,10 @@
+#Write a program that prints the number of characters in a user's name.
+#You may need to google for a function that calculates the length of a string.
+#The len() is used to calculate the length of a string
+#print(len(input("What is your name? ")))
+
+#variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
